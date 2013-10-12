5a8097764eb659f04f5a1c38996bdf615916177b
Matt Parrilla <matthew.parrilla@gmail.com>
2013-10-12 13:24:06 -0400

    Working on affordability

--- js/main.js
+++ js/main.js
@@ -138,6 +138,7 @@ VTH.vtMap.loadData = function(error, vt, data) {
   VTH.calculate_livability_scores();
   VTH.vtMap.render();
 
+  VTH.initLineGraph();
   var excludes = [];
   for (var i = 0; i < vt.objects.vt_towns.geometries.length; i++) {
     if (included.indexOf(vt.objects.vt_towns.geometries[i].properties.town.toUpperCase()) === -1) {
@@ -191,9 +192,7 @@ VTH.select_town = function(town) {
   $('#town-name').text(name);
   $('#town-population').text(addCommas(population));
   $('.info header').css('background-image', 'url('+image+')');
-};
-
-VTH.affordability_chart = function(town) {
+  VTH.updateLineGraph();
 };
 
 VTH.init_menu = function() {
@@ -255,6 +254,76 @@ VTH.calculate_livability_scores = function() {
     feature.properties['livability'] = indicator_total / VTH.livability_weights['total'];
   });
 }
+
+VTH.initLineGraph = function() {
+    var m = [0, 0, 0, 0]; // margins
+    var w = Math.floor($(window).width() * 0.40) - 20 - m[1] - m[3]; // width
+    var h = 200 - m[0] - m[2]; // height
+
+    VTH.graph = d3.select("#affordability-chart").append("svg:svg")
+            .attr("width", w + m[1] + m[3])
+            .attr("height", h + m[0] + m[2])
+        .append("svg:g")
+            .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+    VTH.graph.w = w;
+    VTH.graph.m = m;
+    VTH.graph.h = h;
+
+}
+
+VTH.updateLineGraph = function() {
+    $('#affordability-chart').css('display', 'block');
+
+    // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+    var data = [23011.14, 23444.39,	25297.25, 26018.98, 26391.53, 26279.06, 27354.20, 29421.18,	31036.08, 33925.49,	34161.88, 36642.35,	36320.16, 38330.35];
+
+    // X scale will fit all values from data[] within pixels 0-w
+    var x = d3.scale.linear().domain([0, data.length]).range([0, VTH.graph.w]);
+    // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+    var y = d3.scale.linear().domain([0, 40000]).range([VTH.graph.h, 0]);
+        // automatically determining max range can work something like this
+        // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+    // create a line function that can convert data[] into x and y points
+    var line = d3.svg.line()
+        // assign the X function to plot our line as we wish
+        .x(function(d,i) { 
+            // verbose logging to show what's actually being done
+            console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+            // return the X coordinate where we want to plot this datapoint
+            return x(i); 
+        })
+        .y(function(d) { 
+            // verbose logging to show what's actually being done
+            console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+            // return the Y coordinate where we want to plot this datapoint
+            return y(d); 
+        })
+
+        // Add an SVG element with the desired dimensions and margin.
+        // create yAxis
+        var xAxis = d3.svg.axis().scale(x).tickSize(-VTH.graph.h).tickSubdivide(true);
+        // Add the x-axis.
+        VTH.graph.append("svg:g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(5," + VTH.graph.h - 50 + ")")
+                .call(xAxis);
+
+
+        // create left yAxis
+        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+        // Add the y-axis to the left
+        VTH.graph.append("svg:g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(-25,0)")
+                .call(yAxisLeft);
+        
+        // Add the line by appending an svg:path element with the data line we created above
+        // do this AFTER the axes above so that the line is above the tick-lines
+        VTH.graph.append("svg:path").attr("d", line(data));
+}
+
 
 $(document).ready(function() {
   VTH.init();
