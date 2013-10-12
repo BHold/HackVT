c0d712bb56d1036e6eff25996f6488431d834be5
Brian Holdefehr <bhold45@gmail.com>
2013-10-11 22:00:56 -0400

    can draw cloropleth based on field name

--- js/main.js
+++ js/main.js
@@ -1,37 +1,99 @@
 var VTH = VTH || {};
-VTH.map = {};
+VTH.vtMap = {};
 
 VTH.init = function() {
   queue()
     .defer(d3.json, "vt.json")
-    .await(VTH.map.render);
+    .defer(d3.csv, "data/housing_costs.csv")
+    .await(VTH.vtMap.loadData);
 };
 
-VTH.map.options = {
+VTH.vtMap.options = {
   'width': Math.floor($(window).width() * 0.40 * 0.75),
-  'height': Math.floor($(window).height())
+  'height': Math.floor($(window).height()),
+  'colorRange': ["#fff7ec","#fee8c8","#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f","#b30000","#7f0000"],
+  'fields': ['mun_tax_rate2002', 'mun_tax_rate2003', 'mun_tax_rate2004'],
+  'selectedField': 'mun_tax_rate2004'
 };
 
-VTH.map.svg = d3.select(".vermont").append("svg")
-  .attr("width", VTH.map.options.width)
-  .attr("height", VTH.map.options.height);
+VTH.vtMap.svg = d3.select(".vermont").append("svg")
+  .attr("width", VTH.vtMap.options.width)
+  .attr("height", VTH.vtMap.options.height);
 
-VTH.map.projection = d3.geo.transverseMercator()
+VTH.vtMap.projection = d3.geo.transverseMercator()
   .rotate([72.57, -44.20])
-  .translate([VTH.map.options.width / 2, VTH.map.options.height * 0.3])
+  .translate([VTH.vtMap.options.width / 2, VTH.vtMap.options.height * 0.3])
   .scale([15000]);
 
-VTH.map.path = d3.geo.path()
-  .projection(VTH.map.projection);
+VTH.vtMap.path = d3.geo.path()
+  .projection(VTH.vtMap.projection);
 
-VTH.map.render = function(error, vt) {
-  VTH.map.svg.selectAll(".town")
+VTH.vtMap.render = function() {
+  var vt = VTH.vtMap.data;
+  var field = VTH.vtMap.options.selectedField;
+  var color = VTH.vtMap.getScale(field);
+
+  VTH.vtMap.svg.selectAll(".town")
     .data(topojson.feature(vt, vt.objects.vt_towns).features)
    .enter().append("path")
-    .attr("d", VTH.map.path)
+    .attr("d", VTH.vtMap.path)
     .style("fill", function(d) {
-      return "#ddd";
-    })
+      var stat = d.properties[field];
+
+      if (stat) {
+        return color(stat);
+      } else {
+        return "#ddd";
+      }
+    });
+};
+
+//VTH.vtMap.getY = function(domain) {
+//  return d3.scale.linear()
+//    .domain([0, Math.max(domain)])
+//    .range([0, $(window).height() * 0.7]);
+//};
+//
+//VTH.vtMap.getYAxis = function(domain) {
+//  var y = VTH.vtMap.getY(domain)
+//
+//  return d3.svg.axis()
+//    .scale(y)
+//    .tickValues(color.domain())
+//    .orient("right");
+//};
+
+VTH.vtMap.getDomain = function(field) {
+  var domain = [];
+  for (var i = 0; i < VTH.vtMap.data.objects.vt_towns.geometries.length; i++) {
+    var value = VTH.vtMap.data.objects.vt_towns.geometries[i].properties[field];
+    domain.push(value);
+  }
+  return domain;
+};
+
+VTH.vtMap.getScale = function(field) {
+  return d3.scale.quantile()
+    .domain(VTH.vtMap.getDomain(field))
+    .range(VTH.vtMap.options.colorRange);
+};
+
+VTH.vtMap.loadData = function(error, vt, data) {
+  for (var i = 0; i < data.length; i++) {
+    var dataTown = data[i].town.toUpperCase();
+
+    for (var j = 0; j < vt.objects.vt_towns.geometries.length; j++) {
+      var jsonTown = vt.objects.vt_towns.geometries[j].properties.town.toUpperCase();
+      if (dataTown === jsonTown) {
+        for (var k = 0; k < VTH.vtMap.options.fields.length; k++) {
+          var field = VTH.vtMap.options.fields[k];
+          vt.objects.vt_towns.geometries[j].properties[field] = data[i][field];
+        }
+      }
+    }
+  }
+  VTH.vtMap.data = vt;
+  VTH.vtMap.render();
 };
 
 $(document).ready(function() {
